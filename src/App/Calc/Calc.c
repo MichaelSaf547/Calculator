e6e3034a4b92a9ba42cc25dec0bbf77e90e6ee08
RomarioBasm <romariobasm@gmail.com>
2022-04-15 00:38:49 +0200

    Adjusting in files

--- src/App/Calc/Calc.c
+++ src/App/Calc/Calc.c
@@ -33,131 +33,145 @@ static u8 CalcApp_u8Sign = 0;
 
 void CalcApp_vidTask(){
 
-    static Keypad_tenuErrorStatus Loc_enuErrorStatus = Keypad_enuNotOk;
+            
+        
+    switch(CalcApp_enuCurrentState){ 
+    
+        case CalcApp_enuStartState: 
+             CalcApp_vidStartProcess(Loc_enuErrorStatus);
+        break;
+        
+        case CalcApp_enuGetOperandOneState:
+             CalcApp_vidGetOperandOneProcess(Loc_enuErrorStatus);
+        break;
+        
+        case CalcApp_enuGetOperandTwoState:
+             CalcApp_vidGetOperandTWOProcess(Loc_enuErrorStatus);
+        break;
+    }
+    
+}
+
+static void CalcApp_vidStartProcess(){
+    
+    Keypad_tenuErrorStatus Loc_enuErrorStatus = Keypad_enuNotOk;
     
     Loc_enuErrorStatus = Keypad_enuGetKey(&Loc_u8PressedValue);
     
-    if(Loc_enuErrorStatus == Keypad_enuOk ){        
+    if(Loc_enuErrorStatus == Keypad_enuOk ){ 
         
-        switch(CalcApp_enuCurrentState){ 
-        
-            case CalcApp_enuStartState: 
-                 CalcApp_vidStartProcess(Loc_enuErrorStatus);
-            break;
+        /* GET A DIGIT */ 
+        if(Loc_u8PressedValue >= '0' && Loc_u8PressedValue <= '9'){
             
-            case CalcApp_enuGetOperandOneState:
-                 CalcApp_vidGetOperandOneProcess(Loc_enuErrorStatus);
-            break;
+            /* BUFFER THE OPERATION */
+            Loc_s16CurrOperand = Loc_u8PressedValue;
+            CalcApp_strCurrInputData.CalcApp_strOperand1 = Loc_u8PressedValue; 
             
-            case CalcApp_enuGetOperandTwoState:
-                 CalcApp_vidGetOperandTWOProcess(Loc_enuErrorStatus);
-            break;
-        }
-    }    
-        
-}
-
-static void CalcApp_vidStartProcess(u8 Copy_u8KeyValue){
-    
-    /* GET A DIGIT */ 
-    if(Copy_u8KeyValue >= '0' && Copy_u8KeyValue <= '9'){
-        
-        /* BUFFER THE OPERATION */
-        Loc_s16CurrOperand = Copy_u8KeyValue;
-        CalcApp_strCurrInputData.CalcApp_strOperand1 = Copy_u8KeyValue; 
-        
-        CalcApp_enuCurrentState = CalcApp_enuDisplayOperandOneState;
-    }
-    
-    /* GET MINUS */
-    else if(Copy_u8KeyValue == '-'){
-        
-        /* ASSIGN THE SIGN */
-        CalcApp_u8Sign = 1;
-    }
-    
-    else{
-        /* DO NOTHING */ 
-    }
-                
-}
-static void CalcApp_vidGetOperandOneProcess(u8 Copy_u8KeyValue){
-    
-    if(Copy_u8KeyValue >= '0' && Copy_u8KeyValue <= '9'){
-                    
-        /* BUFFER THE OPERATION */
-        
-        /* MINUS OPERATION */ 
-        if(CalcApp_u8Sign == 1){
-            CalcApp_strCurrInputData.CalcApp_strOperand1 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand1) + Copy_u8KeyValue) * -1;  
-            CalcApp_u8Sign = 0;
+            CalcApp_enuCurrentState = CalcApp_enuDisplayOperandOneState;
         }
         
-        else{
-            CalcApp_strCurrInputData.CalcApp_strOperand1 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand1) + Copy_u8KeyValue);  
-        }
-        
-        /* CHANGE THE CURRENT STATE */ 
-        CalcApp_enuCurrentState = CalcApp_enuDisplayOperandOneState;
-
-    }
-
-    else if(Copy_u8KeyValue == '-' || Copy_u8KeyValue == '+' || Copy_u8KeyValue == '*' || Copy_u8KeyValue == '/'){
-        
-        /* BUFFER THE OPERATION */
-        CalcApp_strCurrInputData.CalcApp_strOperation = Copy_u8KeyValue; 
-        
-        /* CHANGE THE CURRENT STATE */ 
-        CalcApp_enuCurrentState = CalcApp_enuDisplayOperationState;
-    }
-
-    else{
-        /* DO NOTHING */ 
-    }
-}
-static void CalcApp_vidGetOperandTwoProcess(u8 Copy_u8KeyValue){
-    
-    if(Copy_u8KeyValue >= '0' && Copy_u8KeyValue <= '9'){
-                    
-        /* BUFFER THE OPERATION */
-        
-        /* MINUS OPERATION */ 
-        if(CalcApp_u8Sign == 1){
-            CalcApp_strCurrInputData.CalcApp_strOperand2 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand2) + Copy_u8KeyValue) * -1;  
-            CalcApp_u8Sign = 0;
-        }
-        
-        else{
-            CalcApp_strCurrInputData.CalcApp_strOperand2 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand2) + Copy_u8KeyValue);  
-        }
-        
-        /* CHANGE THE CURRENT STATE */ 
-        CalcApp_enuCurrentState = CalcApp_enuDisplayOperandOneState;
-
-    }
-    
-
-    else if(Copy_u8KeyValue == '-' || Copy_u8KeyValue == '+' || Copy_u8KeyValue == '*' || Copy_u8KeyValue == '/'){
-        
-        /* BUFFER THE OPERATION */
-        CalcApp_strCurrInputData.CalcApp_strOperation = Copy_u8KeyValue; 
-        
-        /* CHANGE THE CURRENT STATE */ 
-        CalcApp_enuCurrentState = CalcApp_enuDisplayOperationState;
-    }
-    
-    /* OPERATOR IS ALREADY ASSIGNED */
-    else if(CalcApp_strCurrInputData.CalcApp_strOperation == '-' || CalcApp_strCurrInputData.CalcApp_strOperation == '+' || CalcApp_strCurrInputData.CalcApp_strOperation == '*' ||CalcApp_strCurrInputData.CalcApp_strOperation == '/' ){
-        
-        /* CURRENT OPERATION IS A MINUS */ 
-        if(Copy_u8KeyValue == '-'){
+        /* GET MINUS */
+        else if(Loc_u8PressedValue == '-'){
+            
+            /* ASSIGN THE SIGN */
             CalcApp_u8Sign = 1;
         }
         
-    }
+        else{
+            /* DO NOTHING */ 
+        }
+    }                
+}
+static void CalcApp_vidGetOperandOneProcess(){
+    
+    Keypad_tenuErrorStatus Loc_enuErrorStatus = Keypad_enuNotOk;
+    
+    Loc_enuErrorStatus = Keypad_enuGetKey(&Loc_u8PressedValue);
+    
+    if(Loc_enuErrorStatus == Keypad_enuOk ){ 
+    
+        if(Loc_u8PressedValue >= '0' && Loc_u8PressedValue <= '9'){
+                        
+            /* BUFFER THE OPERATION */
+            
+            /* MINUS OPERATION */ 
+            if(CalcApp_u8Sign == 1){
+                CalcApp_strCurrInputData.CalcApp_strOperand1 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand1) + Loc_u8PressedValue) * -1;  
+                CalcApp_u8Sign = 0;
+            }
+            
+            else{
+                CalcApp_strCurrInputData.CalcApp_strOperand1 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand1) + Loc_u8PressedValue);  
+            }
+            
+            /* CHANGE THE CURRENT STATE */ 
+            CalcApp_enuCurrentState = CalcApp_enuDisplayOperandOneState;
 
-    else{
-        /* DO NOTHING */ 
+        }
+
+        else if(Loc_u8PressedValue == '-' || Loc_u8PressedValue == '+' || Loc_u8PressedValue == '*' || Loc_u8PressedValue == '/'){
+            
+            /* BUFFER THE OPERATION */
+            CalcApp_strCurrInputData.CalcApp_strOperation = Loc_u8PressedValue; 
+            
+            /* CHANGE THE CURRENT STATE */ 
+            CalcApp_enuCurrentState = CalcApp_enuDisplayOperationState;
+        }
+
+        else{
+            /* DO NOTHING */ 
+        }
+    }
+}
+static void CalcApp_vidGetOperandTwoProcess(){
+    
+    Keypad_tenuErrorStatus Loc_enuErrorStatus = Keypad_enuNotOk;
+    
+    Loc_enuErrorStatus = Keypad_enuGetKey(&Loc_u8PressedValue);
+    
+    if(Loc_enuErrorStatus == Keypad_enuOk ){ 
+        if(Loc_u8PressedValue >= '0' && Loc_u8PressedValue <= '9'){
+                        
+            /* BUFFER THE OPERATION */
+            
+            /* MINUS OPERATION */ 
+            if(CalcApp_u8Sign == 1){
+                CalcApp_strCurrInputData.CalcApp_strOperand2 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand2) + Loc_u8PressedValue) * -1;  
+                CalcApp_u8Sign = 0;
+            }
+            
+            else{
+                CalcApp_strCurrInputData.CalcApp_strOperand2 = ((10 * CalcApp_strCurrInputData.CalcApp_strOperand2) + Loc_u8PressedValue);  
+            }
+            
+            /* CHANGE THE CURRENT STATE */ 
+            CalcApp_enuCurrentState = CalcApp_enuDisplayOperandOneState;
+
+        }
+        
+
+        else if(Loc_u8PressedValue == '-' || Loc_u8PressedValue == '+' || Loc_u8PressedValue == '*' || Loc_u8PressedValue == '/'){
+            
+            /* BUFFER THE OPERATION */
+            CalcApp_strCurrInputData.CalcApp_strOperation = Loc_u8PressedValue; 
+            
+            /* CHANGE THE CURRENT STATE */ 
+            CalcApp_enuCurrentState = CalcApp_enuDisplayOperationState;
+        }
+        
+        /* OPERATOR IS ALREADY ASSIGNED */
+        else if(CalcApp_strCurrInputData.CalcApp_strOperation == '-' || CalcApp_strCurrInputData.CalcApp_strOperation == '+' || CalcApp_strCurrInputData.CalcApp_strOperation == '*' ||CalcApp_strCurrInputData.CalcApp_strOperation == '/' ){
+            
+            /* CURRENT OPERATION IS A MINUS */ 
+            if(Loc_u8PressedValue == '-'){
+                CalcApp_u8Sign = 1;
+            }
+            
+        }
+
+        else{
+            /* DO NOTHING */ 
+        }
     }
 }
 
