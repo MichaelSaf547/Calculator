2cb928a86a3888cb4b2bd39dee3ad3a19e1bb928
RomarioBasm <romariobasm@gmail.com>
2022-04-14 23:48:07 +0200

    Adding files: Calc.h Calc.c

--- src/App/Calc/Calc.h
+++ src/App/Calc/Calc.h
@@ -8,6 +8,43 @@
 #ifndef APP_CALC_CALC_H_
 #define APP_CALC_CALC_H_
 
+/* ==================================================================== */
+/* ========================== New Types =============================== */
+/* ==================================================================== */
+typedef struct{
+    
+    s64 CalcApp_strOperand1;
+    s64 CalcApp_strOperand2;
+    u8  CalcApp_strOperation;
+    
+}CalcApp_strInputData;
+
+typedef struct{
+    
+    s64 CalcApp_strResult;
+    
+}CalcApp_strOutputData;
+
+typedef enum{
+    
+    CalcApp_enuStartState, 
+    
+    CalcApp_enuGetOperandOneState,
+    CalcApp_enuDisplayOperandOneState,
+    CalcApp_enuDisplayOperationState,
+    CalcApp_enuGetOperandTwoState,
+    CalcApp_enuDisplayOperandTwoState,
+    CalcApp_enuCalculateResultState,
+    
+    CalcApp_enuDisplayResultState,
+    
+}CalcApp_enuOperandState;
+
+
+/* ==================================================================== */
+/* ===================== Function Prototypes ========================== */
+/* ==================================================================== */
+void CalcApp_vidTask();
 
 
 
